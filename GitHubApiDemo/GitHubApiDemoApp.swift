68c5a540c0c22769a3f60ceee3ddc4124b070a38
j33852 <j33852@gmail.com>
2023-08-14 10:32:28 +0900

    Initial Commit

---
+++ GitHubApiDemo/GitHubApiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApiDemoApp.swift
+//  GitHubApiDemo
+//
+//  Created by 張俊傑 on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
